e8eb9ca1b38a40b097756cd7723674ec636e19c3
timush-a <smtimush@yandex.ru>
2020-04-15 23:30:33 +0300

    Пример использования абстрактного суперкласса

---
+++ streams.py
@@ -0,0 +1,31 @@
+class Processor:
+    """
+    Класс определяет метод converter, который, как ождидается
+    будет переопределн в дочерних классах.
+    Пример использования абстрактного суперкласса
+    """
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    def process(self):
+        while 1:
+            data = self.reader.readline()
+            if not data:
+                break
+            data = self.converter(data)
+            self.writer.write(data)
+
+    def converter(self, data):
+        assert False, 'converter must be defined'
+
+
+class Uppercase(Processor):
+    def converter(self, data):
+        return data.upper()
+
+
+if __name__ == '__main__':
+    import sys
+    obj = Uppercase(open('spam.txt', sys.stdout))
+    obj.process
