dd5c6c17508e9a5e81a2e6eca05aa0995240ba0d
timush-a <smtimush@yandex.ru>
2020-04-16 22:44:43 +0300

    put files in folders

---
+++ chapter_30_operator_overloading/iters.py
@@ -0,0 +1,14 @@
+class Squares:
+    """ Simple class with iterator"""
+    def __init__(self, start, stop):
+        self.value = start - 1
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value == self.stop:
+            raise StopIteration
+        self.value += 1
+        return self.value ** 2
