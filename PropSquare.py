f0dfef3364d3ccba482a5ac9c3606b7ba389e321
Timush Aleksander <50115510+timush-a@users.noreply.github.com>
2020-05-27 16:22:06 +0300

    Create PropSquare.py

---
+++ PropSquare.py
@@ -0,0 +1,19 @@
+class PropSquare:
+    def __init__(self, start):
+        self.value = start
+
+    def getX(self):
+        return self.value ** 2
+
+    def setX(self, value):
+        self.value = value
+    X = property(getX, setX)
+
+
+P = PropSquare(3)
+Q = PropSquare(32)
+
+print(P.X)
+P.X = 4
+print(P.X)
+print(Q.X)
